ad5d1958a64bb7b42f7096f8e200b6398ca3dca5
psyc <me@psyclyx.xyz>
2025-07-12 01:23:50 -0700

    feat(nixos/home-assistant): add port option to HA module

--- modules/nixos/services/home-assistant.nix
+++ modules/nixos/services/home-assistant.nix
@@ -1,26 +1,50 @@
 { config, lib, ... }:
 let
   cfg = config.psyclyx.services.home-assistant;
+  port = config.psyclyx.networking.ports.home-assistant;
 in
 {
   options = {
-    psyclyx.services.home-assistant = {
-      enable = lib.mkEnableOption "Enables Home Assistant, with @psyclyx's config";
+    psyclyx = {
+      services = {
+        home-assistant = {
+          enable = lib.mkEnableOption "Enables Home Assistant, with @psyclyx's config";
+        };
+      };
+      networking = {
+        ports = {
+          home-assistant = lib.mkOption {
+            type = lib.types.port;
+            default = 8123;
+          };
+        };
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
-    services.home-assistant = {
-      enable = true;
-      extraComponents = [
-        "esphome"
-        "met"
-        "radio_browser"
-        "zha"
-        "ios"
-      ];
-      config = {
-        default_config = { };
+    networking = {
+      firewall = {
+        allowedTCPPorts = [ port ];
+      };
+    };
+
+    services = {
+      home-assistant = {
+        enable = true;
+        extraComponents = [
+          "esphome"
+          "met"
+          "radio_browser"
+          "zha"
+          "ios"
+        ];
+        config = {
+          http = {
+            server_port = port;
+          };
+          default_config = { };
+        };
       };
     };
   };
