5a25f4273fe19b5c3f42ce4205da5f8731ae17e1
psyclyx <me@psyclyx.xyz>
2025-07-16 16:09:10 -0700

    emacs: continue iterating on new emacs config

--- pkgs/emacs/default.nix
+++ pkgs/emacs/default.nix
@@ -1,10 +1,47 @@
-{ pkgs }:
+{
+  pkgs,
+  package ? pkgs.emacs-unstable-pgtk,
+}:
 let
-  emacs = pkgs.emacsWithPackagesFromUsePackage {
-    config = ./config.org;
-    defaultInitFile = true;
-    alwaysTangle = true;
-    package = pkgs.emacs-unstable-pgtk;
-  };
+  packages = (
+    epkgs: with epkgs; [
+      better-jumper
+      cape
+      consult
+      corfu
+      evil
+      evil-collection
+      evil-easymotion
+      evil-nerd-commenter
+      evil-snipe
+      evil-surround
+      evil-textobj-anyblock
+      exato
+      general
+      magit
+      marginalia
+      orderless
+      projectile
+      smartparens
+      vertico
+      ws-butler
+      zenburn-theme
+    ]
+  );
+  defaultInitFileName = "default.el";
+  configFile = pkgs.writeText defaultInitFileName (builtins.readFile ./config.org);
+  orgModeConfigFile = pkgs.runCommand defaultInitFileName { nativeBuildInputs = [ package ]; } ''
+    cp ${configFile} config.org
+    emacs -Q --batch ./config.org -f org-babel-tangle
+    mv config.el $out
+  '';
 in
-emacs
+(pkgs.emacsPackagesFor package).emacsWithPackages (
+  epkgs:
+  epkgs.trivialBuild {
+    pname = "default";
+    src = orgModeConfigFile;
+    version = "0.1.0";
+    packageRequires = packages epkgs;
+  }
+)
