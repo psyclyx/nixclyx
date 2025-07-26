e600779821ec17085b5225d31560a0b3256a7e6e
psyclyx <me@psyclyx.xyz>
2025-07-26 14:26:24 -0700

    tidy: style in modules/home/config.nix

--- modules/home/config.nix
+++ modules/home/config.nix
@@ -1,17 +1,13 @@
 { lib, ... }:
 {
-  options = {
-    psyclyx = {
-      user = {
-        name = lib.mkOption {
-          type = lib.types.str;
-          description = "User's full name";
-        };
-        email = lib.mkOption {
-          type = lib.types.str;
-          description = "User's email";
-        };
-      };
+  options.psyclyx.user = {
+    name = lib.mkOption {
+      type = lib.types.str;
+      description = "User's full name";
+    };
+    email = lib.mkOption {
+      type = lib.types.str;
+      description = "User's email";
     };
   };
 }
