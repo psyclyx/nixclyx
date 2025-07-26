f1c4a67b2000c2ea5d20535061d1ed2c7699f34b
psyclyx <me@psyclyx.xyz>
2025-07-26 16:04:41 -0700

    alacritty: remove theme

--- modules/home/programs/alacritty.nix
+++ modules/home/programs/alacritty.nix
@@ -1,29 +1,6 @@
 { config, lib, ... }:
 let
   cfg = config.psyclyx.programs.alacritty;
-  font = {
-    normal = {
-      family = "Aporetic Sans Mono";
-    };
-    size = 16;
-  };
-  doomOne = {
-    primary = {
-      background = "0x282c34";
-      foreground = "0xbbc2cf";
-    };
-    normal = {
-      black = "0x282c34";
-      red = "0xff6c6b";
-      green = "0x98be65";
-      yellow = "0xecbe7b";
-      blue = "0x51afef";
-      magenta = "0xc678dd";
-      cyan = "0x46d9ff";
-      white = "0xbbc2cf";
-    };
-  };
-  colors = doomOne;
 in
 {
   options = {
@@ -43,7 +20,6 @@ in
           window = {
             option_as_alt = "Both";
           };
-          inherit font colors;
         };
       };
     };
