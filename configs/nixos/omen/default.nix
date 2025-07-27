4021435a4f566909a8445ceffdebbd583eb8fb33
psyclyx <me@psyclyx.xyz>
2025-07-27 03:31:12 -0700

    stylix: add font sizes/opacity

--- configs/nixos/omen/default.nix
+++ configs/nixos/omen/default.nix
@@ -19,7 +19,19 @@
   stylix = {
     enable = true;
     image = ../../wallpapers/madoka-homura-2x.png;
+    opacity = {
+      applications = 0.9;
+      desktop = 0.8;
+      terminal = 0.9;
+      popups = 0.8;
+    };
     fonts = {
+      sizes = {
+        desktop = 12;
+        applications = 14;
+        terminal = 14;
+        popups = 14;
+      };
       serif = {
         package = pkgs.nerd-fonts.noto;
         name = "NotoSerif Nerd Font";
