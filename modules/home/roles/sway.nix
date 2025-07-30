6b2538f97df3d111edeafbc97ea562fccaffceea
psyclyx <me@psyclyx.xyz>
2025-07-30 15:30:25 -0700

    tidy: dot syntax in roles/sway.nix

--- modules/home/roles/sway.nix
+++ modules/home/roles/sway.nix
@@ -8,26 +8,12 @@ let
   cfgEnabled = config.psyclyx.roles.sway;
 in
 {
-  options = {
-    psyclyx = {
-      roles = {
-        sway = lib.mkEnableOption "sway, etc";
-      };
-    };
-  };
-
+  options.psyclyx.roles.sway = lib.mkEnableOption "sway, etc";
   config = lib.mkIf cfgEnabled {
     assertions = [ (lib.hm.assertions.assertPlatform "psyclyx.roles.sway" pkgs lib.platforms.linux) ];
-
     psyclyx = {
-      programs = {
-        sway = {
-          enable = lib.mkDefault true;
-        };
-        waybar = {
-          enable = lib.mkDefault true;
-        };
-      };
+      programs.sway.enable = lib.mkDefault true;
+      programs.waybar.enable = lib.mkDefault true;
     };
   };
 }
