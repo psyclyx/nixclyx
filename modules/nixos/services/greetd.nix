63818d5330c2a41c3e5fc3252774153bc70f301c
psyclyx <me@psyclyx.xyz>
2025-07-30 09:46:49 -0700

    switch from tuigreet to regreet

--- modules/nixos/services/greetd.nix
+++ modules/nixos/services/greetd.nix
@@ -6,31 +6,10 @@
 }:
 let
   cfg = config.psyclyx.services.greetd;
-  tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
 in
 {
-  options = {
-    psyclyx = {
-      services = {
-        greetd = {
-          enable = lib.mkEnableOption "Enable greetd greeter.";
-        };
-      };
-    };
-  };
-
+  options.psyclyx.services.greetd.enable = lib.mkEnableOption "greetd+regreet";
   config = lib.mkIf cfg.enable {
-    services = {
-      greetd = {
-        enable = true;
-        vt = 2;
-        settings = {
-          default_session = {
-            command = "${tuigreet} --time --user-menu --remember --asterisks --cmd sway";
-          };
-          user = "greeter";
-        };
-      };
-    };
+    programs.regreet.enable = true;
   };
 }
