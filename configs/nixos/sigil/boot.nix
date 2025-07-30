7f55b05cbcead7ac15494f20f2fa9d2caa0ec923
psyclyx <me@psyclyx.xyz>
2025-07-30 14:51:33 -0700

    untested: kernel and plymouth on sigil

--- configs/nixos/sigil/boot.nix
+++ configs/nixos/sigil/boot.nix
@@ -1,7 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 {
   boot = {
+    kernelPackages = pkgs.linuxPackages_cachyos;
+    binfmt.emulatedSystems = [ "aarch64-linux" ];
+    plymouth.enable = true;
+
+    initrd = {
+      verbose = false;
+      systemd.enable = true;
+    };
+
     loader = {
+      timeout = 0;
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
@@ -10,6 +20,13 @@
       "amd_iommu=on"
       "iommu=pt"
       "nvidia-drm.modeset=1"
+      "quiet"
+      "splash"
+      "intremap=on"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+      "mitigations=off"
     ];
 
     kernelModules = [
