57081ec48003459e358f3f18372d5d1d19148c68
psyclyx <me@psyclyx.xyz>
2025-07-31 21:57:26 -0700

    tidy: format dev role

--- modules/home/roles/dev.nix
+++ modules/home/roles/dev.nix
@@ -8,28 +8,18 @@ let
   cfgEnabled = config.psyclyx.roles.dev;
 in
 {
-  options = {
-    psyclyx = {
-      roles = {
-        dev = lib.mkEnableOption "dev tools/config";
-      };
-    };
-  };
+  options.psyclyx.roles.dev = lib.mkEnableOption "dev tools/config";
 
   config = lib.mkIf cfgEnabled {
-    home = {
-      packages = with pkgs; [
-        zig
-        zls
-        nixfmt-rfc-style
-        nixd
-      ];
-    };
+    home.packages = with pkgs; [
+      zig
+      zls
+      nixfmt-rfc-style
+      nixd
+    ];
     psyclyx = {
       programs = {
-        git = {
-          enable = lib.mkDefault true;
-        };
+        git.enable = lib.mkDefault true;
         emacs.enable = lib.mkDefault true;
       };
     };
