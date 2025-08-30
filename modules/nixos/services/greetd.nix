4a435ae01b6a75ffe52c57e596ccb67f995874b4
psyclyx <me@psyclyx.xyz>
2025-08-30 06:27:58 -0700

    regreet: cage on one monitor, allow vt switching

--- modules/nixos/services/greetd.nix
+++ modules/nixos/services/greetd.nix
@@ -10,6 +10,13 @@ in
 {
   options.psyclyx.services.greetd.enable = lib.mkEnableOption "greetd+regreet";
   config = lib.mkIf cfg.enable {
-    programs.regreet.enable = true;
+    programs.regreet = {
+      enable = true;
+      cageArgs = [
+        "-m"
+        "last"
+        "-s"
+      ];
+    };
   };
 }
