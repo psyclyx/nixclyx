4743c7a6ed161f08eb0c6f999bc607fdebf84d57
psyclyx <me@psyclyx.xyz>
2025-09-11 00:00:28 -0700

    tidy: remove unused rofi packages

--- overlay.nix
+++ overlay.nix
@@ -3,13 +3,10 @@ final: prev:
 let
   inherit (prev.stdenv) isLinux;
   common = {
-    upscale-image = prev.callPackage ./pkgs/upscale-image.nix { };
-    print256colors = prev.callPackage ./pkgs/print256colors.nix { };
+    upscale-image = prev.callPackage ./packages/upscale-image.nix { };
+    print256colors = prev.callPackage ./packages/print256colors.nix { };
   };
-  linux = {
-    rofi = prev.callPackage ./pkgs/rofi { };
-    rofi-session = prev.callPackage ./pkgs/rofi-session.nix { };
-  };
+  linux = { };
 in
 {
   psyclyx = common // (if isLinux then linux else { });
--- pkgs/default.nix
+++
@@ -1,6 +0,0 @@
-pkgs:
-let
-  inherit (pkgs) callPackage stdenv;
-  inherit (stdenv) isDarwin isLinux;
-in
-if isLinux then { rofi = callPackage ./rofi; } else { }
