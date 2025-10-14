0234194711fe5415cb95dce7ac1428bb83a1cd12
psyclyx <me@psyclyx.xyz>
2025-10-14 08:59:21 -0700

    feat, avahi: clean up avahi config
    openFirewall: enabled by default, redundant
    nssmdns: deprecated in favor of nssdns4
    
    not enabling nssmdns6 to avoid long timeouts

--- modules/nixos/services/avahi.nix
+++ modules/nixos/services/avahi.nix
@@ -5,15 +5,14 @@ in
 {
   options = {
     psyclyx.services.avahi = {
-      enable = lib.mkEnableOption "Service discovery / mdns";
+      enable = lib.mkEnableOption "Service discovery / MDNS";
     };
   };
 
   config = lib.mkIf cfg.enable {
     services.avahi = {
       enable = true;
-      nssmdns = true;
-      openFirewall = true;
+      nssmdns4 = true;
     };
   };
 }
