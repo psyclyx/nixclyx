a1397153fed104564f45d60e0ba93720039a55db
psyclyx <me@psyclyx.xyz>
2025-10-25 08:05:18 -0700

    tidy: flake doesn't set nixConfig

--- flake.nix
+++ flake.nix
@@ -21,17 +21,5 @@
     };
   };
 
-  nixConfig = {
-    extra-substituters = [
-      "https://psyclyx.cachix.org?priority=0"
-      "https://nix-community.cachix.org?priority=1"
-    ];
-    extra-trusted-public-keys = [
-      "psyclyx.cachix.org-1:UFwKXEDn3gLxIW9CeXGdFFUzCIjj8m6IdAQ7GA4XfCk="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
-    ];
-  };
-
   outputs = inputs: import ./outputs.nix inputs;
 }
