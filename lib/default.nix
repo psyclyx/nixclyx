e601194d7acdb8cea968d0e3feeba77695b40f77
psyclyx <me@psyclyx.xyz>
2025-11-22 08:49:18 -0800

    remove unused files in lib/

--- lib/default.nix
+++ lib/default.nix
@@ -6,10 +6,6 @@ let
 
   imports = {
     checks = ./checks.nix;
-    darwin = ./darwin.nix;
-    packageSets = ./packageSets.nix;
-    systems = ./systems.nix;
-    test = ./test.nix;
   };
 
   importPath = path: import path { inherit lib psyclyxLib; };
