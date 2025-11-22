a3c800469a4b747d81981f6a13d9f310227e3e18
psyclyx <me@psyclyx.xyz>
2025-11-22 08:47:03 -0800

    modules/darwin/* -> modules/darwin/config, matches nixos + misc cleanup

--- modules/darwin/config/roles/base.nix
+++ modules/darwin/config/roles/base.nix
@@ -23,8 +23,10 @@ in
         home-manager.enable = lib.mkDefault true;
         homebrew.enable = lib.mkDefault true;
         nix.enable = lib.mkDefault true;
+        nixpkgs.enable = lib.mkDefault true;
         security.enable = lib.mkDefault true;
         settings.enable = lib.mkDefault true;
+        stylix.enable = lib.mkDefault true;
       };
     };
   };
--- modules/darwin/config/system/nix.nix
+++ modules/darwin/config/system/nix.nix
@@ -1,17 +1,12 @@
 {
   config,
   lib,
-  inputs,
   ...
 }:
 let
   cfg = config.psyclyx.system.nix;
-
-  inherit (inputs) self;
 in
 {
-  imports = [ self.commonModules.nix ];
-
   config = lib.mkIf cfg.enable {
     nix.settings.trusted-users = [ "@admin" ];
   };
