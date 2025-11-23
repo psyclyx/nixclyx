a6d3c6fa1b58f91a706fca7246ff4508078390d0
psyclyx <me@psyclyx.xyz>
2025-11-23 08:56:29 -0800

    move shell.nix to devShells/default.nix

---
+++ devShells/default.nix
@@ -0,0 +1,16 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+{
+  default = pkgs.mkShell {
+    packages = with pkgs; [
+      age
+      nixfmt-rfc-style
+      nixd
+      nix-tree
+      sops
+      ssh-to-age
+      yq
+    ];
+  };
+}
