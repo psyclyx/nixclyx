b0237696c6a9cce92c6f786b1c0d36330d0b093c
psyclyx <me@psyclyx.xyz>
2025-11-23 08:57:17 -0800

    assets/ gets dirToAttrSet from lib

--- assets/default.nix
+++ assets/default.nix
@@ -1,17 +1,1 @@
-let
-  inherit (builtins) mapAttrs readDir;
-
-  dirToAttrSet =
-    dir:
-    let
-      entryValue =
-        name: type:
-        let
-          name' = "${dir}/${name}";
-          directory = type == "directory";
-        in
-        if directory then dirToAttrSet name' else name';
-    in
-    mapAttrs entryValue (readDir dir);
-in
-dirToAttrSet ./.
+{ psycNix }: psycNix.lib.dirToAttrSet ./.
