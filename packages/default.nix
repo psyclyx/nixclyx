c1004577bffef655c7d17a17353f5ef719a29f7a
psyclyx <me@psyclyx.xyz>
2025-11-25 02:26:13 -0800

    add intelmas package

--- packages/default.nix
+++ packages/default.nix
@@ -1,5 +1,6 @@
 { nixclyx, pkgs, ... }:
 nixclyx.lib.callSupportedPackages pkgs {
+  intelmas = ./intelmas.nix;
   print256colors = ./print256colors.nix;
   ssacli = ./ssacli.nix;
   upscale-image = ./upscale-image;
