a890419701573732f1ed2d43d5eff03d2759faec
psyclyx <me@psyclyx.xyz>
2025-12-01 06:53:48 -0800

    fix, 3dprinting: valid attr name

--- envs/3DPrinting.nix
+++ envs/3DPrinting.nix
@@ -1,8 +1,7 @@
 pkgs:
 pkgs.buildEnv {
-  name = "env-3dprinting";
+  name = "env-3DPrinting";
   paths = [
-    pkgs.freecad-wayland
     pkgs.orca-slicer
   ];
   meta.description = "3D printing and CAD software";
--- envs/default.nix
+++ envs/default.nix
@@ -1,22 +1,9 @@
+{ pkgs ? import <nixpkgs> { }, ... }:
 {
-  pkgs ? import <nixpkgs> { },
-}:
-let
-  # Import shell and language environments
-  shell = import ./shell pkgs;
+  _3DPrinting = import ./3DPrinting.nix pkgs;
+  forensics = import ./forensics.nix pkgs;
   languages = import ./languages pkgs;
-
-  # Import standalone environments
   llm = import ./llm.nix pkgs;
   media = import ./media.nix pkgs;
-  "3dprinting" = import ./3dprinting.nix pkgs;
-  forensics = import ./forensics.nix pkgs;
-in
-{
-  # Expose shell and languages at top level
-  inherit shell languages;
-
-  # Expose standalone environments
-  inherit llm media forensics;
-  "3dprinting" = "3dprinting";
+  shell = import ./shell pkgs;
 }
