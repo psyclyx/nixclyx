22555715acf16ef9b2590527ab5a351c809133f3
psyclyx <me@psyclyx.xyz>
2025-12-16 02:53:14 -0800

    nixos: add preservation module

--- flake.nix
+++ flake.nix
@@ -3,27 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:psyclyx/nixpkgs/psyclyx";
+
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     disko.url = "github:nix-community/disko";
 
     colmena.url = "github:zhaofengli/colmena";
 
     sops-nix.url = "github:Mic92/sops-nix";
+
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
     stylix = {
       url = "github:psyclyx/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    ghostty = {
-      url = "github:ghostty-org/ghostty";
-    };
+
+    preservation.url = "github:nix-community/preservation";
+
+    ghostty.url = "github:ghostty-org/ghostty";
+
     zsh-pure = {
       url = "github:sindresorhus/pure";
       flake = false;
