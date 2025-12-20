0cd4ab183d2dc810289c7051b8140085f88bfc0e
psyclyx <me@psyclyx.xyz>
2025-12-20 01:47:36 -0800

    remove disclyx

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,5 @@
 { nixclyx, pkgs, ... }:
 nixclyx.lib.callSupportedPackages pkgs {
-  disclyx = ./disclyx;
   intelmas = ./intelmas.nix;
   print256colors = ./print256colors.nix;
   ssacli = ./ssacli.nix;
