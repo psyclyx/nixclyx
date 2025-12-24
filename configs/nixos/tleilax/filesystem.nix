72430724c08968cbf35c9ca99d50f7112dcc2e89
psyclyx <me@psyclyx.xyz>
2025-12-24 09:42:47 -0800

    tleilax: remove all but root in preparation for bcachefs

--- configs/nixos/tleilax/filesystem.nix
+++ configs/nixos/tleilax/filesystem.nix
@@ -29,58 +29,5 @@
         };
       };
     };
-    disk.disk2 = {
-      device = "/dev/disk/by-id/ata-CT2000MX500SSD1_2403E88EF894";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          root = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/var/lib/misc";
-            };
-          };
-        };
-      };
-    };
-
-    disk.disk3 = {
-      device = "/dev/disk/by-id/ata-Samsung_SSD_870_EVO_2TB_S753NL0Y104917Y";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          root = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/var/lib/containers";
-            };
-          };
-        };
-      };
-    };
-
-    disk.disk4 = {
-      device = "/dev/disk/by-id/ata-Samsung_SSD_870_EVO_2TB_S753NL0Y105029W";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          root = {
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/var/lib/misc2";
-            };
-          };
-        };
-      };
-    };
   };
 }
