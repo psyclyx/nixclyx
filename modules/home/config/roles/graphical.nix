a4ad46969ec515ca12edd4e4ecdb06551e38e34c
psyclyx <me@psyclyx.xyz>
2025-12-29 14:50:00 -0800

    style: inherit (lib) in roles/graphical.nix

--- modules/home/config/roles/graphical.nix
+++ modules/home/config/roles/graphical.nix
@@ -2,41 +2,45 @@
   config,
   lib,
   pkgs,
-  inputs,
   ...
 }:
 let
+  inherit (lib)
+    mkDefault
+    mkEnableOption
+    mkIf
+    mkMerge
+    ;
   cfg = config.psyclyx.roles.graphical;
 in
 {
   options = {
     psyclyx.roles.graphical = {
-      enable = lib.mkEnableOption "Graphical session programs and configuration";
+      enable = mkEnableOption "Graphical session programs and configuration";
     };
   };
 
-  config = lib.mkIf cfg.enable (
-    lib.mkMerge [
-      (lib.mkIf pkgs.stdenv.isLinux {
-        home.packages = with pkgs; [
-          firefox-bin
-          signal-desktop-bin
-        ];
-        psyclyx = {
-          programs = {
-            alacritty.enable = lib.mkDefault true;
-            ghostty = {
-              enable = lib.mkDefault true;
-              defaultTerminal = lib.mkDefault true;
-            };
-            sway.enable = lib.mkDefault true;
+  config = mkIf cfg.enable (mkMerge [
+    (mkIf pkgs.stdenv.isLinux {
+      home.packages = with pkgs; [
+        element-desktop
+        firefox-bin
+        signal-desktop-bin
+      ];
+      psyclyx = {
+        programs = {
+          alacritty.enable = mkDefault true;
+          ghostty = {
+            enable = mkDefault true;
+            defaultTerminal = mkDefault true;
           };
-          xdg.enable = lib.mkDefault true;
+          sway.enable = mkDefault true;
         };
-      })
-      (lib.mkIf pkgs.stdenv.isDarwin {
-        psyclyx.programs.kitty.enable = lib.mkDefault true;
-      })
-    ]
-  );
+        xdg.enable = mkDefault true;
+      };
+    })
+    (mkIf pkgs.stdenv.isDarwin {
+      psyclyx.programs.kitty.enable = mkDefault true;
+    })
+  ]);
 }
