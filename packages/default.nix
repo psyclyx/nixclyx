2d37bb4ba10125acc026a7bf75450e26b3979c12
psyclyx <me@psyclyx.xyz>
2026-01-04 16:25:51 -0800

    pkgs: remove broken intelmas package

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,5 @@
 { nixclyx, pkgs, ... }:
 nixclyx.lib.callSupportedPackages pkgs {
-  intelmas = ./intelmas.nix;
   print256colors = ./print256colors.nix;
   ssacli = ./ssacli.nix;
   upscale-image = ./upscale-image;
