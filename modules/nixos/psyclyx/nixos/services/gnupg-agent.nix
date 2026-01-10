18675dccc0d05f5a4c32a63c6cbcc328aceabe6f
psyclyx <me@psyclyx.xyz>
2026-01-09 19:15:19 -0800

    nixos: add gnupg agent service

---
+++ modules/nixos/psyclyx/nixos/services/gnupg-agent.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.psyclyx.nixos.services.gnupg-agent;
+in
+{
+  options = {
+    psyclyx.nixos.services.gnupg-agent = {
+      enable = lib.mkEnableOption "gnupg agent (for pinentry)";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.gnupg.agent = {
+      enable = true;
+    };
+  };
+}
