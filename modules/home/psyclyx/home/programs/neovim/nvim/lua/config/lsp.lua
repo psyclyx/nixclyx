6c202e6388566b0d2df7a090531e2ce9116a33e8
psyclyx <me@psyclyx.xyz>
2026-01-19 07:36:30 -0800

    fix, neovim: enable LSP before files open, not after

--- modules/home/psyclyx/home/programs/neovim/nvim/lua/config/lsp.lua
+++ modules/home/psyclyx/home/programs/neovim/nvim/lua/config/lsp.lua
@@ -56,21 +56,8 @@ local servers = {
 
 for server, config in pairs(servers) do
   vim.lsp.config(server, config)
+  vim.lsp.enable(server)
 end
-
-vim.api.nvim_create_autocmd('FileType', {
-  callback = function(args)
-    local bufnr = args.buf
-    local ft = vim.bo[bufnr].filetype
-
-    for server, config in pairs(servers) do
-      if config.filetypes and vim.tbl_contains(config.filetypes, ft) then
-        vim.lsp.enable(server)
-        break
-      end
-    end
-  end,
-})
 
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
@@ -78,19 +65,26 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     local opts = { buffer = bufnr, silent = true }
 
-    vim.keymap.set('n', '<leader>cd', vim.lsp.buf.definition, vim.tbl_extend('force', opts, { desc = 'Go to definition' }))
-    vim.keymap.set('n', '<leader>cD', vim.lsp.buf.type_definition, vim.tbl_extend('force', opts, { desc = 'Go to type definition' }))
+    vim.keymap.set('n', '<leader>cd', vim.lsp.buf.definition,
+      vim.tbl_extend('force', opts, { desc = 'Go to definition' }))
+    vim.keymap.set('n', '<leader>cD', vim.lsp.buf.type_definition,
+      vim.tbl_extend('force', opts, { desc = 'Go to type definition' }))
     vim.keymap.set('n', '<leader>cr', vim.lsp.buf.references, vim.tbl_extend('force', opts, { desc = 'Find references' }))
-    vim.keymap.set('n', '<leader>ci', vim.lsp.buf.implementation, vim.tbl_extend('force', opts, { desc = 'Go to implementation' }))
+    vim.keymap.set('n', '<leader>ci', vim.lsp.buf.implementation,
+      vim.tbl_extend('force', opts, { desc = 'Go to implementation' }))
     vim.keymap.set('n', '<leader>cn', vim.lsp.buf.rename, vim.tbl_extend('force', opts, { desc = 'Rename symbol' }))
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, vim.tbl_extend('force', opts, { desc = 'Code action' }))
     vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, vim.tbl_extend('force', opts, { desc = 'Format code' }))
     vim.keymap.set('n', '<leader>ch', vim.lsp.buf.hover, vim.tbl_extend('force', opts, { desc = 'Hover documentation' }))
-    vim.keymap.set('n', '<leader>ck', vim.lsp.buf.signature_help, vim.tbl_extend('force', opts, { desc = 'Signature help' }))
+    vim.keymap.set('n', '<leader>ck', vim.lsp.buf.signature_help,
+      vim.tbl_extend('force', opts, { desc = 'Signature help' }))
 
-    vim.keymap.set('n', '<leader>ce', vim.diagnostic.open_float, vim.tbl_extend('force', opts, { desc = 'Show diagnostics' }))
-    vim.keymap.set('n', '<leader>c[', vim.diagnostic.goto_prev, vim.tbl_extend('force', opts, { desc = 'Previous diagnostic' }))
-    vim.keymap.set('n', '<leader>c]', vim.diagnostic.goto_next, vim.tbl_extend('force', opts, { desc = 'Next diagnostic' }))
+    vim.keymap.set('n', '<leader>ce', vim.diagnostic.open_float,
+      vim.tbl_extend('force', opts, { desc = 'Show diagnostics' }))
+    vim.keymap.set('n', '<leader>c[', vim.diagnostic.goto_prev,
+      vim.tbl_extend('force', opts, { desc = 'Previous diagnostic' }))
+    vim.keymap.set('n', '<leader>c]', vim.diagnostic.goto_next,
+      vim.tbl_extend('force', opts, { desc = 'Next diagnostic' }))
 
     if client and client.supports_method('textDocument/formatting') then
       vim.api.nvim_create_autocmd('BufWritePre', {
