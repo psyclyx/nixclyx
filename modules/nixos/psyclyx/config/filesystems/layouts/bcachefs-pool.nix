dabdd7dacc3d31bb754762607cf7fc36537fe2ee
psyclyx <me@psyclyx.xyz>
2026-01-26 03:21:43 -0800

    nixos: move filesystems.layouts under psyclyx.nixos

--- modules/nixos/psyclyx/config/filesystems/layouts/bcachefs-pool.nix
+++ modules/nixos/psyclyx/config/filesystems/layouts/bcachefs-pool.nix
@@ -1,12 +1,12 @@
 { config, lib, ... }:
 let
-  cfg = config.psyclyx.filesystems.layouts.bcachefs-pool;
+  cfg = config.psyclyx.nixos.filesystems.layouts.bcachefs-pool;
 in
 {
   options = {
     # TODO: Not the best abstraction, some sort of simpler wrapper around
     # fileSystems with a spot to compose bcachefs-specific stuff would be nicer
-    psyclyx.filesystems.layouts.bcachefs-pool = {
+    psyclyx.nixos.filesystems.layouts.bcachefs-pool = {
       enable = lib.mkEnableOption "@psyclyx's bcachefs-pool disk layout";
       wants = lib.mkOption {
         type = lib.types.listOf lib.types.str;
