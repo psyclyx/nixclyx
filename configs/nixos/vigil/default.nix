6c3a181b879e1b360824d869a4f620f7f1ebee83
psyclyx <me@psyclyx.xyz>
2026-01-31 18:41:03 -0800

    vigil: update filesystems

--- configs/nixos/vigil/default.nix
+++ configs/nixos/vigil/default.nix
@@ -1,48 +1,19 @@
-{ ... }:
-{
+{...}: {
   config = {
     networking.hostName = "vigil";
-
-    fileSystems =
-      let
-        fs = {
-          device = "/dev/disk/by-uuid/b45981b5-dd30-416f-89cc-a1eea9a0a9fc";
-          fsType = "bcachefs";
-        };
-      in
-      {
-        "/" = fs // {
-          options = [ "X-mount.subdir=/subvolumes/root" ];
-        };
-
-        "/fs" = fs;
-
-        "/nix" = fs // {
-          options = [ "X-mount.subdir=/subvolumes/nix" ];
-          neededForBoot = true;
-        };
-
-        "/persist" = fs // {
-          options = [ "X-mount.subdir=/subvolumes/persist" ];
-          neededForBoot = true;
-        };
-
-        "/var/log" = fs // {
-          options = [ "X-mount.subdir=/subvolumes/log" ];
-          neededForBoot = true;
-        };
-
-        "/boot" = {
-          device = "/dev/disk/by-uuid/0289-61AC";
-          fsType = "vfat";
-          options = [ "umask=0077" ];
-        };
-      };
 
     psyclyx = {
       nixos = {
         boot = {
           initrd-ssh.enable = true;
+        };
+
+        filesystems.layouts.bcachefs-pool = {
+          enable = true;
+          UUID = {
+            root = "0b6d93c8-c6d3-4243-9413-25543a093c65";
+            boot = "0289-61AC";
+          };
         };
 
         hardware = {
