8043509dc3a6789446c4eeba272b453d58fd90a3
psyclyx <me@psyclyx.xyz>
2026-02-06 04:19:58 -0800

    feat: add clj-nix for Clojure builds
    clj-nix provides lock file based dependency management, eliminating
    the need to manually track dependency hashes.

--- overlay.nix
+++ overlay.nix
@@ -4,9 +4,11 @@ let
 
   colmena = loadFlake sources.colmena;
   astal = loadFlake sources.astal;
+  clj-nix = loadFlake sources.clj-nix;
 in
   _: prev: {
     psyclyx = import ./packages {pkgs = prev;};
     colmena = colmena.packages.${prev.stdenv.hostPlatform.system};
     astal = astal.packages.${prev.stdenv.hostPlatform.system};
+    clj-nix = clj-nix.packages.${prev.stdenv.hostPlatform.system};
   }
