f25b504ff793f473146420e710029e27b318fff1
psyclyx <me@psyclyx.xyz>
2026-02-09 17:03:28 -0800

    feat(network): add bond module and extend networkd with disableInterfaces
    New declarative bond module for LACP bonds via systemd-networkd.
    Add disableInterfaces option to networkd for forcing interfaces down.

--- modules/nixos/options/network/networkd.nix
+++ modules/nixos/options/network/networkd.nix
@@ -1,11 +1,26 @@
 {
   path = ["psyclyx" "nixos" "network" "networkd"];
   description = "systemd-networkd";
-  config = {lib, ...}: {
-    networking.useNetworkd = true;
-    systemd.network = {
-      enable = true;
-      wait-online.enable = lib.mkDefault false;
+  options = {lib, ...}: {
+    disableInterfaces = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+      description = "Interface names to force down (sets ActivationPolicy=down).";
     };
   };
+  config = {cfg, lib, ...}: lib.mkMerge [
+    {
+      networking.useNetworkd = true;
+      systemd.network = {
+        enable = true;
+        wait-online.enable = lib.mkDefault false;
+      };
+    }
+    (lib.mkIf (cfg.disableInterfaces != []) {
+      systemd.network.networks."20-disable" = {
+        matchConfig.Name = builtins.concatStringsSep " " cfg.disableInterfaces;
+        linkConfig.ActivationPolicy = "down";
+      };
+    })
+  ];
 }
