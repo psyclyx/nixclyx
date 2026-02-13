8958c05d14273c528aebfed8c4122d43303c224a
psyclyx <me@psyclyx.xyz>
2026-02-13 04:35:41 -0800

    tidy(network): directly set systemd.network

--- modules/nixos/options/network/networkd.nix
+++ modules/nixos/options/network/networkd.nix
@@ -1,31 +1,14 @@
 {
   path = ["psyclyx" "nixos" "network" "networkd"];
   description = "systemd-networkd";
-  options = {lib, ...}: {
-    disableInterfaces = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = [];
-      description = "Interface names to force down (sets ActivationPolicy=down).";
-    };
-  };
   config = {
     cfg,
     lib,
     ...
-  }:
-    lib.mkMerge [
-      {
-        networking.useNetworkd = true;
-        systemd.network = {
-          enable = true;
-          wait-online.enable = lib.mkDefault false;
-        };
-      }
-      (lib.mkIf (cfg.disableInterfaces != []) {
-        systemd.network.networks."20-disable" = {
-          matchConfig.Name = builtins.concatStringsSep " " cfg.disableInterfaces;
-          linkConfig.ActivationPolicy = "down";
-        };
-      })
-    ];
+  }: {
+    networking.useNetworkd = true;
+    systemd.network = {
+      enable = true;
+    };
+  };
 }
