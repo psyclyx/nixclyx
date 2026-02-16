11b7334122957b8e661e59474c093588a625e932
psyclyx <me@psyclyx.xyz>
2026-02-16 01:22:10 -0800

    feat(nvf): consolidate config and add missing essentials
    Merge scattered nvf module files into core.nix and add telescope,
    blink-cmp, lualine, autopairs, comment-nvim, enableLSP, nvim-paredit
    for structural editing, and spacemacs-style window/buffer keybinds.

--- modules/nvf/core.nix
+++ modules/nvf/core.nix
@@ -11,6 +11,7 @@
   config = {
     cfg,
     lib,
+    pkgs,
     ...
   }: {
     vim = {
@@ -57,7 +58,197 @@
 
       theme = {
         enable = true;
+        name = "base16";
+        transparent = true;
       };
+
+      diagnostics = {
+        enable = true;
+      };
+      filetree.neo-tree.enable = true;
+      binds = {
+        whichKey.enable = true;
+      };
+
+      dashboard.alpha.enable = true;
+
+      formatter.conform-nvim.enable = true;
+      git.enable = true;
+      telescope.enable = true;
+      autocomplete.blink-cmp.enable = true;
+      statusline.lualine.enable = true;
+
+      languages = {
+        enableDAP = true;
+        enableExtraDiagnostics = true;
+        enableFormat = true;
+        enableLSP = true;
+        enableTreesitter = true;
+        assembly.enable = true;
+        bash.enable = true;
+        clojure.enable = true;
+        clang.enable = true;
+        haskell.enable = true;
+        html.enable = true;
+        java.enable = true;
+        json.enable = true;
+        julia.enable = true;
+        just.enable = true;
+        lua.enable = true;
+        markdown.enable = true;
+        nix.enable = true;
+        ocaml.enable = true;
+        odin.enable = true;
+        python.enable = true;
+        rust = {
+          enable = true;
+          extensions.crates-nvim.enable = true;
+        };
+        sql.enable = true;
+        ts.enable = true;
+        wgsl.enable = true;
+        yaml.enable = true;
+        zig.enable = true;
+      };
+
+      treesitter = {
+        autotagHtml = true;
+        context.enable = true;
+        fold = true;
+        textobjects.enable = true;
+      };
+      ui = {
+        borders = {
+          enable = true;
+          globalStyle = "single";
+        };
+        breadcrumbs = {
+          enable = true;
+          navbuddy.enable = true;
+        };
+        colorizer.enable = true;
+        illuminate.enable = true;
+        noice = {
+          enable = true;
+          setupOpts.lsp.signature.enabled = true;
+        };
+      };
+      autopairs.nvim-autopairs.enable = true;
+      comments.comment-nvim.enable = true;
+      utility = {
+        ccc.enable = true;
+        direnv.enable = true;
+        mkdir.enable = true;
+        motion.precognition.enable = true;
+        multicursors.enable = true;
+        surround.enable = true;
+      };
+      repl.conjure.enable = true;
+      visuals = {
+        rainbow-delimiters.enable = true;
+      };
+      assistant.avante-nvim.enable = true;
+
+      extraPlugins = {
+        nvim-paredit = {
+          package = pkgs.vimPlugins.nvim-paredit;
+          setup = ''require("nvim-paredit").setup({})'';
+        };
+      };
+
+      keymaps = [
+        # window management
+        {
+          mode = "n";
+          key = "<leader>wv";
+          action = "<cmd>vsplit<cr>";
+          desc = "Vertical split";
+        }
+        {
+          mode = "n";
+          key = "<leader>ws";
+          action = "<cmd>split<cr>";
+          desc = "Horizontal split";
+        }
+        {
+          mode = "n";
+          key = "<leader>wd";
+          action = "<cmd>close<cr>";
+          desc = "Close window";
+        }
+        {
+          mode = "n";
+          key = "<leader>w=";
+          action = "<C-w>=";
+          desc = "Balance windows";
+        }
+        {
+          mode = "n";
+          key = "<leader>wh";
+          action = "<C-w>h";
+          desc = "Window left";
+        }
+        {
+          mode = "n";
+          key = "<leader>wj";
+          action = "<C-w>j";
+          desc = "Window down";
+        }
+        {
+          mode = "n";
+          key = "<leader>wk";
+          action = "<C-w>k";
+          desc = "Window up";
+        }
+        {
+          mode = "n";
+          key = "<leader>wl";
+          action = "<C-w>l";
+          desc = "Window right";
+        }
+
+        # buffer management
+        {
+          mode = "n";
+          key = "<leader>bd";
+          action = "<cmd>bdelete<cr>";
+          desc = "Delete buffer";
+        }
+        {
+          mode = "n";
+          key = "<leader>bn";
+          action = "<cmd>bnext<cr>";
+          desc = "Next buffer";
+        }
+        {
+          mode = "n";
+          key = "<leader>bp";
+          action = "<cmd>bprevious<cr>";
+          desc = "Previous buffer";
+        }
+        {
+          mode = "n";
+          key = "<leader><tab>";
+          action = "<cmd>b#<cr>";
+          desc = "Last buffer";
+        }
+
+        # quit/session
+        {
+          mode = "n";
+          key = "<leader>qq";
+          action = "<cmd>qa<cr>";
+          desc = "Quit all";
+        }
+
+        # search
+        {
+          mode = "n";
+          key = "<leader>sc";
+          action = "<cmd>nohlsearch<cr>";
+          desc = "Clear highlights";
+        }
+      ];
 
       luaConfigRC.anthropic-key = lib.mkIf (cfg.anthropicKeyFile != null) (lib.nvim.dag.entryAnywhere ''
         local f = io.open("${cfg.anthropicKeyFile}", "r")
