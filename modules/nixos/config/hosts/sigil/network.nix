1f2fccd3fad2a8124f9d8acaa7530d415f7a0341
psyclyx <me@psyclyx.xyz>
2026-02-16 11:16:42 -0800

    feat(sigil): configure bond0 LACP network aggregation

--- modules/nixos/config/hosts/sigil/network.nix
+++ modules/nixos/config/hosts/sigil/network.nix
@@ -4,19 +4,45 @@
   ...
 }: {
   config = lib.mkIf (config.psyclyx.nixos.host == "sigil") {
-    systemd.network.networks."20-sfp" = {
-      matchConfig.Name = "enp5s0f?";
-      networkConfig = {
-        Domains = ["~." "~psyclyx.xyz"];
-        DHCP = "yes";
+    systemd.network = {
+      netdevs = {
+        "10-bond0" = {
+          netdevConfig = {
+            Name = "bond0";
+            Kind = "bond";
+          };
+          bondConfig = {
+            Mode = "802.3ad";
+            LACPTransmitRate = "fast";
+            TransmitHashPolicy = "layer3+4";
+            MIIMonitorSec = "1s";
+          };
+        };
       };
-      dhcpV4Config = {
-        UseDNS = true;
-        UseRoutes = true;
-        RouteMetric = 100;
-      };
-      dhcpV6Config = {
-        UseDNS = true;
+
+      networks = {
+        "20-bond0-ports" = {
+          matchConfig.Name = "enp5s0f?";
+          networkConfig = {
+            Bond = "bond0";
+          };
+        };
+        "20-bond0" = {
+          matchConfig.Name = "bond0";
+          networkConfig = {
+            DHCP = "yes";
+          };
+        };
+        # "20-sfp" = {
+        #   dhcpV4Config = {
+        #     UseDNS = true;
+        #     UseRoutes = true;
+        #     RouteMetric = 100;
+        #   };
+        #   dhcpV6Config = {
+        #     UseDNS = true;
+        #   };
+        # };
       };
     };
   };
