a26a693a16654122584e262f35758007e6042286
psyclyx <me@psyclyx.xyz>
2026-02-17 08:25:38 -0800

    feat(kea): add hooks-libraries option

--- modules/nixos/options/services/kea.nix
+++ modules/nixos/options/services/kea.nix
@@ -12,6 +12,11 @@
       default = [];
       description = "Kea subnet4 configuration objects.";
     };
+    hooksLibraries = lib.mkOption {
+      type = lib.types.listOf lib.types.attrs;
+      default = [];
+      description = "Kea hooks-libraries configuration objects.";
+    };
   };
   config = {cfg, ...}: {
     services.kea.dhcp4 = {
@@ -27,6 +32,7 @@
         renew-timer = 900;
         rebind-timer = 1800;
         subnet4 = cfg.subnets;
+        hooks-libraries = cfg.hooksLibraries;
       };
     };
   };
