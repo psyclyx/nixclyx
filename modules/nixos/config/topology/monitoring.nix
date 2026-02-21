69da601bec8bf4185c652505e135a2a8d31e7d09
psyclyx <me@psyclyx.xyz>
2026-02-21 09:54:37 -0800

    feat(prometheus): use hostnames for scrape targets and add lab hosts
    Switch VPN hosts (tleilax, iyr, sigil) from IP-based to hostname-based
    targets via psyclyx.net DNS. Add lab-1 through lab-4 as scrape targets
    using their rack-vpn hostnames.

--- modules/nixos/config/topology/monitoring.nix
+++ modules/nixos/config/topology/monitoring.nix
@@ -6,10 +6,17 @@
   topo = config.psyclyx.topology;
 
   vpnHosts = lib.filterAttrs (_: host: host.vpn != null) topo.hosts;
+  labHosts = lib.filterAttrs (_: host: host ? labIndex) topo.hosts;
 
-  scrapeTargets = lib.mapAttrsToList
-    (_: host: "${host.vpn.address}:9100")
+  vpnTargets = lib.mapAttrsToList
+    (name: _: "${name}.${topo.domain.internal}:9100")
     vpnHosts;
+
+  labTargets = lib.mapAttrsToList
+    (name: _: "${name}.rack-vpn.${topo.conventions.homeDomain}:9100")
+    labHosts;
+
+  scrapeTargets = vpnTargets ++ labTargets;
 in {
   config = lib.mkIf config.psyclyx.nixos.services.prometheus.server.enable {
     psyclyx.nixos.services.prometheus.server.scrapeTargets = scrapeTargets;
