924ad4043d29e0a9404650955bce931f3290f7f7
psyclyx <me@psyclyx.xyz>
2026-04-03 17:48:34 -0700

    fix(lab): set MACAddressPolicy=none on bond netdev
    systemd-networkd's default MACAddressPolicy=persistent generates a
    random-but-stable MAC for bond interfaces, breaking LACP negotiation
    with the CSS326 since the switch expects the real NIC MAC.

--- hosts/nixos/lab-shared.nix
+++ hosts/nixos/lab-shared.nix
@@ -22,54 +22,110 @@
     )
     me.addresses;
 
-  mkNetworkUnit = netName: _addr: let
+  # Bond slave devices (1G NICs).
+  bondSlaves = ["eno1" "eno2" "eno3" "eno4"];
+
+  # VLANs carried over the bond.
+  bondVlans = [
+    { name = "main";  id = 10; }
+    { name = "infra"; id = 25; }
+    { name = "stage"; id = 31; }
+  ];
+
+  # Networks that live directly on a physical device (not the bond).
+  directNetworks = lib.filterAttrs (name: _:
+    !builtins.elem name (map (v: v.name) bondVlans)
+  ) hostNetworks;
+
+  mkDirectNetworkUnit = netName: _addr: let
     device = me.interfaces.${netName}.device;
     addr = me.addresses.${netName};
     net = eg.entities.${netName}.attrs;
     prefixLen = toString net.prefixLen;
-    isDefault = netName == "infra";
   in {
     matchConfig.Name = device;
     address =
       ["${addr.ipv4}/${prefixLen}"]
       ++ lib.optional (addr.ipv6 != null) "${addr.ipv6}/64";
-    routes = lib.optional isDefault {
-      Gateway = net.gateway4;
-    };
+    networkConfig.IPv6AcceptRA = true;
+    linkConfig.RequiredForOnline = "no";
+  };
+
+  directNetworkUnits = lib.mapAttrs' (netName: addr:
+    lib.nameValuePair "20-${me.interfaces.${netName}.device}" (mkDirectNetworkUnit netName addr)
+  ) directNetworks;
+
+  # Bond VLAN network units.
+  mkVlanNetworkUnit = vlan: let
+    addr = me.addresses.${vlan.name};
+    net = eg.entities.${vlan.name}.attrs;
+    prefixLen = toString net.prefixLen;
+    isDefault = vlan.name == "infra";
+  in {
+    matchConfig.Name = "bond0.${toString vlan.id}";
+    address =
+      ["${addr.ipv4}/${prefixLen}"]
+      ++ lib.optional (addr.ipv6 != null) "${addr.ipv6}/64";
+    routes = lib.optional isDefault { Gateway = net.gateway4; };
     dns = lib.optional isDefault net.gateway4;
     networkConfig.IPv6AcceptRA = true;
     linkConfig.RequiredForOnline =
-      if isDefault
-      then "routable"
-      else "no";
+      if isDefault then "routable" else "no";
   };
 
-  networkUnits =
-    lib.mapAttrs' (
-      netName: addr:
-        lib.nameValuePair "20-${me.interfaces.${netName}.device}" (mkNetworkUnit netName addr)
-    )
-    hostNetworks;
+  vlanNetworkUnits = builtins.listToAttrs (map (vlan:
+    lib.nameValuePair "20-bond0.${toString vlan.id}" (mkVlanNetworkUnit vlan)
+  ) bondVlans);
+
+  # Bond netdev + network + slave + VLAN netdev units (shared between initrd and real system).
+  bondNetdevs = {
+    "10-bond0" = {
+      netdevConfig = { Name = "bond0"; Kind = "bond"; };
+      extraConfig = "[Link]\nMACAddressPolicy=none";
+      bondConfig = { Mode = "802.3ad"; MIIMonitorSec = "0.1"; TransmitHashPolicy = "layer2+3"; };
+    };
+  } // builtins.listToAttrs (map (vlan:
+    lib.nameValuePair "10-bond0.${toString vlan.id}" {
+      netdevConfig = { Name = "bond0.${toString vlan.id}"; Kind = "vlan"; };
+      vlanConfig.Id = vlan.id;
+    }
+  ) bondVlans);
+
+  bondNetworks = {
+    "10-bond-slaves" = {
+      matchConfig.Name = lib.concatStringsSep " " bondSlaves;
+      networkConfig.Bond = "bond0";
+      linkConfig.RequiredForOnline = "enslaved";
+    };
+    "10-bond0" = {
+      matchConfig.Name = "bond0";
+      networkConfig.VLAN = map (v: "bond0.${toString v.id}") bondVlans;
+      linkConfig.RequiredForOnline = "carrier";
+    };
+  };
 
   allDevices =
-    lib.mapAttrsToList (netName: _: me.interfaces.${netName}.device)
-    hostNetworks;
+    (map (v: "bond0.${toString v.id}") bondVlans)
+    ++ lib.mapAttrsToList (netName: _: me.interfaces.${netName}.device) directNetworks;
 
-  infraDevice = me.interfaces.infra.device;
   infraAddr = me.addresses.infra;
   infraNet = eg.entities.infra.attrs;
 in {
   boot.initrd = {
-    kernelModules = ["igb" "tg3"];
-    systemd.network.networks."20-${infraDevice}" = {
-      matchConfig.Name = infraDevice;
-      address = ["${infraAddr.ipv4}/${toString infraNet.prefixLen}"];
-      routes = [{Gateway = infraNet.gateway4;}];
-      linkConfig.RequiredForOnline = "routable";
+    kernelModules = ["igb" "tg3" "bonding" "8021q"];
+    systemd.network.netdevs = bondNetdevs;
+    systemd.network.networks = bondNetworks // {
+      "20-bond0.25" = {
+        matchConfig.Name = "bond0.25";
+        address = ["${infraAddr.ipv4}/${toString infraNet.prefixLen}"];
+        routes = [{ Gateway = infraNet.gateway4; }];
+        linkConfig.RequiredForOnline = "routable";
+      };
     };
   };
 
-  systemd.network.networks = networkUnits;
+  systemd.network.netdevs = bondNetdevs;
+  systemd.network.networks = bondNetworks // vlanNetworkUnits // directNetworkUnits;
 
   psyclyx.nixos.network.firewall = {
     zones.lan.interfaces = allDevices ++ ["wg0"];
@@ -107,6 +163,11 @@ in {
         enable = true;
         dataNetwork = "infra";
         clusterNodes = labHostNames;
+        tls = let secrets = config.services.kubernetes.secretsPath; in {
+          certFile = "${secrets}/etcd.pem";
+          keyFile = "${secrets}/etcd-key.pem";
+          caFile = "${secrets}/ca.pem";
+        };
       };
       patroni = {
         enable = true;
@@ -120,6 +181,10 @@ in {
       };
       openbao-pki.enable = true;
       openbao-kv.enable = true;
+      icecream = {
+        enable = true;
+        schedulerHost = "10.0.25.11"; # lab-1 infra
+      };
       kubernetes = {
         enable = true;
         clusterNodes = labHostNames;
