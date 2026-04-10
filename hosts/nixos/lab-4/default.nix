de4242382089fd08a74a107479c5d9227d305b09
psyclyx <me@psyclyx.xyz>
2026-04-10 14:03:54 -0700

    refactor(lab-4): switch to bcachefs-subvols, extract filesystems to dedicated file

--- hosts/nixos/lab-4/default.nix
+++ hosts/nixos/lab-4/default.nix
@@ -1,11 +1,8 @@
 { lib, ... }: {
-  imports = [ ../lab-shared.nix ];
+  imports = [
+    ../lab-shared.nix
+    ./filesystems.nix
+  ];
 
   networking.hostName = "lab-4";
-  psyclyx.nixos.filesystems.layouts.zfs-pool = {
-    enable = true;
-    hostId = "9924de5f";
-    boot.UUID = "CF86-D5FF";
-    arc.maxBytes = 85899345920; # 80GB
-  };
 }
