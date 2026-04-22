1de1d6f0f2064fb4a47eb3a7cae4e1b751ba518b
psyclyx <me@psyclyx.xyz>
2026-04-22 03:54:23 -0700

    Revert "fix(river): update for upstream Zig 0.16 and wlroots 0.20"
    This reverts commit 22225b490418a3af9dbed4e3a8ef7c10dc8ce912.

--- packages/river/default.nix
+++ packages/river/default.nix
@@ -2,7 +2,7 @@
   lib,
   stdenv,
   callPackage,
-  zig_0_16,
+  fetchurl,
   libGL,
   libx11,
   libevdev,
@@ -16,17 +16,30 @@
   wayland-protocols,
   wayland-scanner,
   xwayland,
-  wlroots_0_20,
+  zig_0_15,
+  wlroots_0_19,
   withManpages ? true,
   xwaylandSupport ? true,
 }:
 
 let
   sources = import ../../npins;
+
+  # River 0.4-dev requires xkb_keymap_get_as_string2, added in xkbcommon 1.12.0.
+  # Nixpkgs has 1.11.0, so override to 1.13.1.
+  libxkbcommon' = libxkbcommon.overrideAttrs (old: rec {
+    version = "1.13.1";
+    src = fetchurl {
+      url = "https://github.com/xkbcommon/libxkbcommon/archive/refs/tags/xkbcommon-${version}.tar.gz";
+      hash = "sha256-rrlRlkwvfswIF0y1UXli0VdZXp4/OPxKEwuR3C+f7Bg=";
+    };
+    patches = [];
+    doCheck = false;
+  });
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "river";
-  version = "0.5.0-dev";
+  version = "0.4.0-dev";
 
   src = sources.river;
 
@@ -37,7 +50,7 @@ stdenv.mkDerivation (finalAttrs: {
       pkg-config
       wayland-scanner
       xwayland
-      zig_0_16
+      zig_0_15
     ]
     ++ lib.optional withManpages scdoc;
 
@@ -46,12 +59,12 @@ stdenv.mkDerivation (finalAttrs: {
       libGL
       libevdev
       libinput
-      libxkbcommon
+      libxkbcommon'
       pixman
       udev
       wayland
       wayland-protocols
-      wlroots_0_20
+      wlroots_0_19
     ]
     ++ lib.optional xwaylandSupport libx11;
 
@@ -69,7 +82,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   meta = {
     homepage = "https://codeberg.org/river/river";
-    description = "Dynamic tiling Wayland compositor (0.5.0-dev)";
+    description = "Dynamic tiling Wayland compositor (0.4.0-dev)";
     license = lib.licenses.gpl3Only;
     platforms = lib.platforms.linux;
     mainProgram = "river";
--- packages/shoal/default.nix
+++ packages/shoal/default.nix
@@ -1,8 +1,2 @@
 { callPackage }:
-let
-  shoal-src = (import ../../npins).shoal;
-  shoal-npins = import "${shoal-src}/npins";
-in
-callPackage "${shoal-src}/package.nix" {
-  snail-src = shoal-npins.snail;
-}
+callPackage "${(import ../../npins).shoal}/package.nix" {}
