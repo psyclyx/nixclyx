a8b50686c9ea1c84d12f1bcc7808ff216e0826af
psyclyx <me@psyclyx.xyz>
2026-04-24 15:31:16 -0700

    fix(shoal): pass snail dependency via npins

--- packages/shoal/default.nix
+++ packages/shoal/default.nix
@@ -1,2 +1,8 @@
 { callPackage }:
-callPackage "${(import ../../npins).shoal}/package.nix" {}
+let
+  shoal-src = (import ../../npins).shoal;
+  shoal-npins = import "${shoal-src}/npins";
+in
+callPackage "${shoal-src}/package.nix" {
+  snail-src = shoal-npins.snail;
+}
