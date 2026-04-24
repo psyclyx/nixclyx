2f7ba7a894c7b1abd53694bee89b126c3086cfd9
psyclyx <me@psyclyx.xyz>
2026-04-24 16:21:17 -0700

    refactor: generalize bcachefs-subvols layout, adopt for sigil and lab-4
    Replace partlabel-specific options (rootPartlabel, bootPartlabel,
    swapPartlabel) with generic device/bootDevice in fstab syntax.
    Add baseMount for bind-mount pattern (avoids duplicate bcachefs
    unlock prompts) and extraDeviceWants for multi-device pools.
    
    Migrate sigil and lab-4 from hand-written filesystems.nix to the
    module. All four bcachefs hosts now use baseMount = "/.fs".

--- hosts/nixos/sigil/filesystems.nix
+++ hosts/nixos/sigil/filesystems.nix
@@ -1,48 +1,38 @@
-{...}: let
-  fsUUID = "055d9737-c13b-4262-abe6-2ebcb8681307";
-  bootUUID = "71AE-12DD";
-  device = "UUID=${fsUUID}";
-
-  wants = [
-    "/dev/disk/by-id/ata-WDC_WDS500G2B0A-00SM50_194894802985"
-    "/dev/disk/by-id/ata-ST4000NM0035-1V4107_ZC12M6AQ"
-  ];
-
-  wantOpts = map (d: "x-systemd.wants=${d}") wants;
-
-  bindMount = subdir: {
-    device = "/.fs/subvolumes/${subdir}/@live";
-    fsType = "none";
-    options = ["bind"];
-  };
-in {
+{ ... }:
+{
   psyclyx.nixos.filesystems.bcachefs.enable = true;
 
-  fileSystems = {
-    "/" = {
-      inherit device;
-      fsType = "bcachefs";
-      options = wantOpts ++ ["X-mount.subdir=subvolumes/root/@live"];
-      neededForBoot = true;
-    };
-
-    "/.fs" = {
-      inherit device;
-      fsType = "bcachefs";
-      options = wantOpts;
-      neededForBoot = true;
-    };
-
-    "/nix" = bindMount "nix" // { neededForBoot = true; };
-    "/persist" = bindMount "persist" // { neededForBoot = true; };
-    "/var/log" = bindMount "log" // { neededForBoot = true; };
-    "/home/psyc" = bindMount "home_psyc";
-    "/root" = bindMount "home_root";
-
-    "/boot" = {
-      device = "UUID=${bootUUID}";
-      fsType = "vfat";
-      options = ["umask=0077"];
+  psyclyx.nixos.filesystems.layouts.bcachefs-subvols = {
+    enable = true;
+    device = "UUID=055d9737-c13b-4262-abe6-2ebcb8681307";
+    bootDevice = "UUID=71AE-12DD";
+    extraDeviceWants = [
+      "/dev/disk/by-id/ata-WDC_WDS500G2B0A-00SM50_194894802985"
+      "/dev/disk/by-id/ata-ST4000NM0035-1V4107_ZC12M6AQ"
+    ];
+    baseMount = "/.fs";
+    subvolumes = {
+      "/" = {
+        subdir = "subvolumes/root/@live";
+      };
+      "/nix" = {
+        subdir = "subvolumes/nix/@live";
+        neededForBoot = true;
+      };
+      "/persist" = {
+        subdir = "subvolumes/persist/@live";
+        neededForBoot = true;
+      };
+      "/var/log" = {
+        subdir = "subvolumes/log/@live";
+        neededForBoot = true;
+      };
+      "/home/psyc" = {
+        subdir = "subvolumes/home_psyc/@live";
+      };
+      "/root" = {
+        subdir = "subvolumes/home_root/@live";
+      };
     };
   };
 }
