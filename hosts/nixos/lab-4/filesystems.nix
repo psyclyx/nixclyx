6bec6dc4705d978b72a408e79d12535d42b292d3
psyclyx <me@psyclyx.xyz>
2026-05-05 04:08:04 -0700

    chore(lab-4): migrate filesystems from bcachefs-subvols to zfs-pool
    Bring lab-4 in line with lab-{1,2,3} (zfs-pool with 128GB ARC).

--- hosts/nixos/lab-4/filesystems.nix
+++ hosts/nixos/lab-4/filesystems.nix
@@ -1,46 +1,9 @@
 { ... }:
 {
-  psyclyx.nixos.filesystems.bcachefs.enable = true;
-
-  psyclyx.nixos.filesystems.layouts.bcachefs-subvols = {
+  psyclyx.nixos.filesystems.layouts.zfs-pool = {
     enable = true;
-    device = "UUID=c9ad48a0-aa58-4734-a7d5-3e6797621405";
-    bootDevice = "UUID=CF86-D5FF";
-    extraDeviceWants = [
-      "/dev/disk/by-partlabel/lab4-ssd"
-      "/dev/disk/by-partlabel/lab4-ssd-2"
-      "/dev/disk/by-partlabel/lab4-ssd-3"
-      "/dev/disk/by-partlabel/lab4-hdd"
-      "/dev/disk/by-partlabel/lab4-hdd-2"
-      "/dev/disk/by-partlabel/lab4-hdd-3"
-      "/dev/disk/by-partlabel/lab4-hdd-4"
-      "/dev/disk/by-partlabel/lab4-hdd-5"
-    ];
-    subvolumes = {
-      "/" = {
-        subdir = "subvolumes/root/@live";
-      };
-      "/nix" = {
-        subdir = "subvolumes/nix/@live";
-        neededForBoot = true;
-      };
-      "/persist" = {
-        subdir = "subvolumes/persist/@live";
-        neededForBoot = true;
-      };
-      "/var/log" = {
-        subdir = "subvolumes/log/@live";
-        neededForBoot = true;
-      };
-      "/var/lib/postgresql" = {
-        subdir = "subvolumes/postgresql/@live";
-      };
-      "/var/lib/redis" = {
-        subdir = "subvolumes/redis/@live";
-      };
-      "/srv/seaweedfs" = {
-        subdir = "subvolumes/seaweedfs/@live";
-      };
-    };
+    hostId = "6fa90ede";
+    boot.UUID = "CF86-D5FF";
+    arc.maxBytes = 137438953472; # 128GB
   };
 }
